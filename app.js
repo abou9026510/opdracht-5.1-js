982f35f3f92726e0a3b68e9582744fca5cc434a1
abou9026510 <9026510@student.zadkine.nl>
2024-10-10 15:28:38 +0200

    opdracht 5.1 js
    opdracht 5.1 js

---
+++ app.js
@@ -0,0 +1,5 @@
+window.onload= function() {
+    var HeadingToAdjust = document.getElementById  (Testheader2);
+    HeadingToAdjust.style.color = "Orange";
+    document.getElementById("Testheader1").innerHTML = "New Tekst!"; 
+};
